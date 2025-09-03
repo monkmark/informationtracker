fee14a2776958df4fbf4025d5c1a6584e7d1bd05
Marcus <mparchman97@gmail.com>
2025-09-03 15:17:57 -0700

    another commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello worrld");
+const div = document.getElementById("messagespace");
+
+//FETCH JSON DATA //
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        //process and display json here
+        for (const key in data) {
+            const element = document.createElement('p');
+            element.innerHTML = `<strong>${key}:</strong>${data[key]}`;
+            div.appendChild(element);
+        }
+    })
+    .catch(error => console.error('error fetching JSON:', error))
